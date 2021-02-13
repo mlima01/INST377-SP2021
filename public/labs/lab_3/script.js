f42a74251ee9c8196fa361c6c205c5ee8161ebd9
mlima <mlima@umd.edu>
2021-02-12 23:33:52 -0500

    Update script.js

--- public/labs/lab_3/script.js
+++ public/labs/lab_3/script.js
@@ -1,1 +1,10 @@
 /* Put your javascript in here */
+
+let listElems = document.querySelectorAll('.pics img')
+const array1 = Array.from(listElems)
+
+let position = 0;
+
+carousel.querySelector('.arrowprev').onclick = function() {
+    position += width * 2;
+}
