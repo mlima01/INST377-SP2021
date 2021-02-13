85242e88138e84e950964c26ccb4dd6bc21836cc
mlima <mlima@umd.edu>
2021-02-13 00:21:33 -0500

    Update script.js

--- public/labs/lab_3/script.js
+++ public/labs/lab_3/script.js
@@ -1,9 +1,39 @@
 /* Put your javascript in here */
-document.addEventListener('onclick',(event)=>{
+document.addEventListener('onclick', (event) => {
   const listElems = document.querySelectorAll('.pics img');
   const array1 = Array.from(listElems);
-  const next = document.querySelector('.arrownext')
-  const prev = document.querySelector('.arrowprev')
+  let currentpos = 0;
+  const width = 130;
+  const count = 3;
+  const pos = width * count;
+  const next = document.querySelector('.arrownext');
+  const prev = document.querySelector('.arrowprev');
 
+  
+  function myFunction1(current) {
+    currentpos += pos;
+    current.style.marginLeft = `${currentpos}px`;
+  }
+  function nextFunc() {
+    array1.forEach(myFunction1);
+  }
 
-})
+  
+  function myFunction2(current) {
+    currentpos -= pos;
+    current.style.marginLeft = `${currentpos}px`;
+  }
+  function prevFunc() {
+    array1.forEach(myFunction2);
+  }
+
+  next.addEventListener('click', () => {
+    // go to next slide on click of the button
+    nextFunc();
+  });
+
+  prev.addEventListener('click', () => {
+    // go to previous slide on click of the button
+    prevFunc();
+  });
+});
