218c7e596179a7236dea0c1248bf793d9067de5f
mlima <mlima@umd.edu>
2021-02-16 17:53:40 -0500

    Update script.js

--- public/labs/lab_3/script.js
+++ public/labs/lab_3/script.js
@@ -1,6 +1,6 @@
 /* Put your javascript in here */
-document.addEventListener('onclick', () => {
-  const listElems = document.querySelectorAll('.pics img');
+document.addEventListener('click', (event) => {
+  const listElems = document.querySelectorAll('li');
   const array1 = Array.from(listElems);
   let currentpos = 0;
   const width = 130;
@@ -8,31 +8,29 @@ document.addEventListener('onclick', () => {
   const pos = width * count;
   const next = document.querySelector('.arrownext');
   const prev = document.querySelector('.arrowprev');
- 
+
+  function myFunction2() {
+    currentpos -= pos;
+    li.style.marginLeft = `${currentpos}px`;
+  }
+  function prevFunc() {
+    array1.forEach(myFunction2());
+  }
+  prev.addEventListener('click', () => {
+    // go to previous slide on click of the button
+    prevFunc();
+  });
+
   function myFunction1() {
     currentpos += pos;
-    img.style.marginLeft = `${currentpos}px`;
+    li.style.marginRight = `${currentpos}px`;
   }
   function nextFunc() {
     array1.forEach(myFunction1());
   }
 
-  
-  function myFunction2() {
-    currentpos -= pos;
-    img.style.marginLeft = `${currentpos}px`;
-  }
-  function prevFunc() {
-    array1.forEach(myFunction2());
-  }
-
   next.addEventListener('click', () => {
     // go to next slide on click of the button
     nextFunc();
-  });
-
-  prev.addEventListener('click', () => {
-    // go to previous slide on click of the button
-    prevFunc();
   });
 });
