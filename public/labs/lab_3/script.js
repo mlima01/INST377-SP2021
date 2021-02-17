0fa8f0e7aeaf8c883856a594a8588c9526d46f98
mlima <mlima@umd.edu>
2021-02-16 20:46:07 -0500

    Update script.js

--- public/labs/lab_3/script.js
+++ public/labs/lab_3/script.js
@@ -1,8 +1,25 @@
 const listElems = document.querySelectorAll('li');
 const array1 = Array.from(listElems);
-
+let list = document.querySelectorAll('ul');
 const width = 130;
 const count = 3;
-let pos = width * count;
+let pos = 0;
 const next = document.querySelector('.carousel__button--next');
 const prev = document.querySelector('.carousel__button--prev');
+
+array1.map(
+  li.style.position = 'relative'
+  && li.insertAdjacentHTML('beforeend',
+    ("<span style='position:absolute;left:0;top:0'></span>"))
+);
+
+function moveNext() {
+  pos -= width * count;
+  pos = Math.min(position, 0);
+  list.style.marginLeft = `${pos}px`;
+}
+
+next.addEventListener('click', () => {
+  // go to next slide on click of the button
+  moveNext();
+});
