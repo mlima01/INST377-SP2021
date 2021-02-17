b2c7f33422ea5e1ab580499d130a551ca7da513d
mlima <mlima@umd.edu>
2021-02-16 21:20:40 -0500

    Update script.js

--- public/labs/lab_3/script.js
+++ public/labs/lab_3/script.js
@@ -1,25 +1,20 @@
-const listElems = document.querySelectorAll('li');
-const array1 = Array.from(listElems);
-let list = document.querySelectorAll('ul');
+const listElems = document.querySelectorAll('li').map(
+  li.style.position = 'relative'
+  && (li.insertAdjacentHTML('beforeend',
+    "<span style= 'position:absolute;left:0;top:0'></span>"))
+);
+
+const list = document.querySelectorAll('ul');
 const width = 130;
 const count = 3;
 let pos = 0;
-const next = document.querySelector('.carousel__button--next');
-const prev = document.querySelector('.carousel__button--prev');
-
-array1.map(
-  li.style.position = 'relative'
-  && li.insertAdjacentHTML('beforeend',
-    ("<span style='position:absolute;left:0;top:0'></span>"))
-);
+const next = document.getElementById('carousel__button--next');
+const prev = document.getElementById('carousel__button--prev');
 
 function moveNext() {
   pos -= width * count;
-  pos = Math.min(position, 0);
+  pos = Math.max(pos, -width * (listElems.length - count));
   list.style.marginLeft = `${pos}px`;
 }
 
-next.addEventListener('click', () => {
-  // go to next slide on click of the button
-  moveNext();
-});
+next.addEventListener('click', moveNext(), false);
