5e1bdbc30dfc7cffe59d3b3a6b66b4e1e9698a16
mlima <mlima@umd.edu>
2021-02-16 20:21:12 -0500

    Update script.js

--- public/labs/lab_3/script.js
+++ public/labs/lab_3/script.js
@@ -1,36 +1,8 @@
-/* Put your javascript in here */
-document.addEventListener('click', (event) => {
-  const listElems = document.querySelectorAll('li');
-  const array1 = Array.from(listElems);
-  let currentpos = 0;
-  const width = 130;
-  const count = 3;
-  const pos = width * count;
-  const next = document.querySelector('.carousel__button--next');
-  const prev = document.querySelector('.carousel__button--prev');
+const listElems = document.querySelectorAll('li');
+const array1 = Array.from(listElems);
 
-  function myFunction2() {
-    currentpos -= pos;
-    li.style.marginLeft = `${currentpos}px`;
-  }
-  function prevFunc() {
-    array1.forEach(myFunction2());
-  }
-  prev.addEventListener('click', () => {
-    // go to previous slide on click of the button
-    prevFunc();
-  });
-
-  function myFunction1() {
-    currentpos += pos;
-    li.style.marginRight = `${currentpos}px`;
-  }
-  function nextFunc() {
-    array1.forEach(myFunction1());
-  }
-
-  next.addEventListener('click', () => {
-    // go to next slide on click of the button
-    nextFunc();
-  });
-});
+const width = 130;
+const count = 3;
+let pos = width * count;
+const next = document.querySelector('.carousel__button--next');
+const prev = document.querySelector('.carousel__button--prev');
