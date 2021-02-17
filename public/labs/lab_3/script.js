993129d4db4cf7423c6c4a751369a8b23be7c69b
mlima <mlima@umd.edu>
2021-02-16 23:02:54 -0500

    Update script.js

--- public/labs/lab_3/script.js
+++ public/labs/lab_3/script.js
@@ -1,33 +1,41 @@
 /* eslint-disable no-console */
 // const listElems = document.querySelectorAll('li');
-function myFunction (item, index) {
-  // eslint-disable-next-line no-param-reassign
-  item.style.position = 'relative';
-  item.insertAdjacentHTML('beforeend',
-    `<span style= 'position:absolute;left:0;top:0'> ${index + 1}</span>`);
-}
+// function myFunction (item, index) {
+//   // eslint-disable-next-line no-param-reassign
+//   item.style.position = 'relative';
+//   item.insertAdjacentHTML('beforeend',
+//     `<span style= 'position:absolute;left:0;top:0'> ${index + 1}</span>`);
+// }
 
 const listElems = document.querySelectorAll('.pics li');
-listElems.forEach(myFunction);
+// listElems.forEach(myFunction);
 
-const width = 130;
+const width = 140;
 const count = 3;
 let pos = 0;
 // const next = document.getElementById('carousel__button--next');
 // const prev = document.getElementById('carousel__button--prev');
+const list = document.querySelectorAll('ul');
+function myFunction2 (item) {
+  // eslint-disable-next-line no-param-reassign
+  item.style.marginLeft = `${pos}px`;
+}
 
-document.getElementById('carousel__button--next').onclick = function moveNext() {
-  const list = document.querySelectorAll('.pics ul');
-
+function moveNext() {
+  // const list = document.querySelectorAll('.pics ul');
+  list.forEach(myFunction2);
   pos -= width * count;
   pos = Math.max(pos, -width * (listElems.length - count));
-  list.style.marginLeft = `${pos}px`;
-};
+}
 
-document.getElementById('carousel__button--prev').onclick = function movePrev() {
-  const list = document.querySelectorAll('.pics ul');
+function movePrev() {
+  list.forEach(myFunction2);
   pos += width * count;
   pos = Math.min(pos, 0);
-  console.log(pos);
-  list.style.marginLeft = `${pos}px`;
-};
+}
+
+// next.addEventListener('click', moveNext(), false);
+// prev.addEventListener('click', movePrev(), false);
+
+document.querySelector('carousel__button--next').onclick = moveNext();
+document.querySelector('carousel__button--prev').onclick = movePrev();
