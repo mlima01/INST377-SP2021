e2ef67230aff619d489bfbbcf46089f25805eba1
mlima <mlima@umd.edu>
2021-02-18 23:29:07 -0500

    Update script.js

---
+++ public/flappy/script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const bird = document.querySelector('.bird');
+  const gameDisplay = document.querySelector('.game-container');
+  const ground = document.querySelector('.ground');
+});
