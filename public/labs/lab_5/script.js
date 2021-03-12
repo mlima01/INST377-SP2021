02ff0b8ec05a3fb3cca64e12e0d45184a3bca567
mlima <mlima@umd.edu>
2021-03-12 17:01:13 -0500

    Update script.js

--- public/labs/lab_5/script.js
+++ public/labs/lab_5/script.js
@@ -1,7 +1,14 @@
 function mapInit() {
-    var mymap = L.map('mapid').setView([51.505, -0.09], 13);
-
-    return mymap;
+  const mymap = L.map('mapid').setView([51.505, -0.09], 13);
+  L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoibWVsaTAxIiwiYSI6ImNrbTZ0cW9idjByZ2YycHVzcGVreXFjZXUifQ.pz0ZvuXltg88sWbXeo33ag'
+}).addTo(mymap);
+  return mymap;
 }
 
 async function dataHandler(mapObjectFromFunction) {
