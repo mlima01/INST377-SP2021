3e06ad479e2f0f464e28a4dce060214c9dcec3d1
mlima <mlima@umd.edu>
2021-03-30 22:32:58 -0400

    Update script.js

--- public/doodle/script.js
+++ public/doodle/script.js
@@ -25,24 +25,24 @@ function createDoodler() {
 }
 
 class Platform {
-    constructor(newPlatBottom){
-        this.bottom = newPlatBottom
-        this.left = Math.random() * 315
-        this.visual = document.createElement('div')
+  constructor(newPlatBottom) {
+    this.bottom = newPlatBottom;
+    this.left = Math.random() * 315;
+    this.visual = document.createElement('div');
 
-        const visual = this.visual
-        visual.classList.add('platform')
-        visual.style.left = this.left + 'px'
-        visual.style.bottom = this.bottom + 'px'
-    }
+    const {visual} = this;
+    visual.classList.add('platform');
+    visual.style.left = `${this.left}px`;
+    visual.style.bottom = `${this.bottom}px`;
+    grid.appendChild(visual);
+  }
 }
 
 function createPlatforms() {
   for (let i = 0; i < platformCount; i++) {
-    let platformSpace = 600 / platformCount;
-    let newPlatBottom = 100 + i * platGap
-    let newPlatform = new Platform()
-
+    const platformSpace = 600 / platformCount;
+    const newPlatBottom = 100 + i * platGap;
+    const newPlatform = new Platform();
   }
 }
 
