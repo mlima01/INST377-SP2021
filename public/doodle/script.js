78758b8537ad57617b916e73cf0a984661304cc9
mlima <mlima@umd.edu>
2021-03-30 21:41:20 -0400

    Update script.js

---
+++ public/doodle/script.js
@@ -0,0 +1,27 @@
+const { create } = require("cypress/types/lodash");
+
+const grid = document.querySelector('.grid');
+const doodler = document.createElement('div');
+const isGameOver = false;
+const speed = 3;
+const platformCount = 5;
+const platforms = [];
+const score = 0;
+const doodlerLeftSpace = 50;
+const startPoint = 150;
+const doodlerBottomSpace = startPoint;
+const gravity = 0.9;
+let upTimerId;
+let downTimerId;
+const isJumping = true;
+const isGoingLeft = false;
+const isGoingRight = false;
+let leftTimerId;
+let rightTimerId;
+
+function createDoodler(){
+    grid.appendChild(doodler)
+    doodler.classList.add('doodler')
+}
+createDoodler()
+
