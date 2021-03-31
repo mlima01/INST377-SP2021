8ae13ae8fdbea579f482227836adfc9442f760cc
mlima <mlima@umd.edu>
2021-03-30 21:47:42 -0400

    Update script.js

--- public/doodle/script.js
+++ public/doodle/script.js
@@ -22,5 +22,8 @@ let rightTimerId;
 function createDoodler() {
   grid.appendChild(doodler);
   doodler.classList.add('doodler');
+  doodler.style.left = doodlerLeftSpace + 'px';
+  doodler.style.bottom = doodlerBottomSpace + 'px';
+  
 }
 createDoodler();
